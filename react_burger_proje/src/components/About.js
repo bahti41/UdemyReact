db77919596cd17cf551be265216c2d1de0ed26b7
bahti41 <sonmezbahtiyar4@gmail.com>
2024-05-29 01:10:59 +0300

    Burger Package site is okey

--- react_burger_proje/src/components/About.js
+++ react_burger_proje/src/components/About.js
@@ -1,9 +1,33 @@
 import React from "react";
+import BannerImage from "../image/banneryeni.jpg";
+import "../style/About.css";
 
 function About() {
   return (
     <>
-      <div>about</div>
+      <div className="about">
+        <div
+          className="aboutTop"
+          style={{ backgroundImage: `url(${BannerImage})` }}
+        ></div>
+        <div className="aboutBottom">
+          <h1>Hakkımzıda</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua Lorem ipsum dolor
+            sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua .
+          </p>
+        </div>
+      </div>
     </>
   );
 }
--- react_burger_proje/src/components/Contact.js
+++ react_burger_proje/src/components/Contact.js
@@ -1,9 +1,39 @@
 import React from "react";
+import Banner from "../image/banner.png";
+import "../style/Contact.css";
 
 function Contact() {
   return (
     <>
-      <div>contact</div>
+      <div className="contact">
+        <div
+          className="leftSide"
+          style={{ backgroundImage: `url(${Banner})` }}
+        ></div>
+        <div className="rightSide">
+          <h1>Bizimle İletişime Geçin</h1>
+          <form>
+            <label>Ad Soyad</label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Lütfen Adınızı ve Soyadınızı Giriniz..."
+            />
+            <label>Email</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Lütfen Email Adresinizi Giriniz..."
+            />
+            <label>Email</label>
+            <textarea
+              rows="6"
+              name="message"
+              placeholder="Lütfen Mesajınızı Giriniz..."
+            ></textarea>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
