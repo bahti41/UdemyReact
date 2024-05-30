8408980dded743eeaddf628c1b0a03fa0babad35
bahti41 <sonmezbahtiyar4@gmail.com>
2024-05-30 19:09:28 +0300

    coklu selector işlemleri başlancak

--- Material_UI_React/materialui/src/App.tsx
+++ Material_UI_React/materialui/src/App.tsx
@@ -2,13 +2,23 @@ import React from "react";
 import "./App.css";
 import LessonTypography from "./components/LessonTypography";
 import LessonButton from "./components/LessonButton";
+import LessonButtonGroup from "./components/LessonButtonGroup";
+import LessonTextFiled from "./components/LessonTextFiled";
+import LessonRadioGroup from "./components/LessonRadioGroup";
+import LessonRadioExemple from "./components/LessonRadioExemple";
+import LessonSelect from "./components/LessonSelect";
 
 function App() {
   return (
     <>
       <div className="App">
         {/* <LessonTypography /> */}
-        <LessonButton />
+        {/* <LessonButton /> */}
+        {/* <LessonButtonGroup /> */}
+        {/* <LessonTextFiled /> */}
+        {/* <LessonRadioGroup /> */}
+        {/* <LessonRadioExemple /> */}
+        <LessonSelect />
       </div>
     </>
   );
